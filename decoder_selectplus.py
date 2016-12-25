b6f5c4c8bfc4fff0ad4b9b8451c0a9ebec05b402
Martijn Berntsen <mxberntsen@gmail.com>
2016-12-25 14:04:17 +0100

    initial commit

---
+++ decoder_selectplus.py
@@ -0,0 +1,150 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+##################################################
+# GNU Radio Python Flow Graph
+# Title: Top Block
+# Generated: Fri Dec 23 10:35:25 2016
+##################################################
+
+if __name__ == '__main__':
+    import ctypes
+    import sys
+    if sys.platform.startswith('linux'):
+        try:
+            x11 = ctypes.cdll.LoadLibrary('libX11.so')
+            x11.XInitThreads()
+        except:
+            print "Warning: failed to XInitThreads()"
+
+from gnuradio import analog
+from gnuradio import blocks
+from gnuradio import eng_notation
+from gnuradio import filter
+from gnuradio import gr
+from gnuradio import wxgui
+from gnuradio.eng_option import eng_option
+from gnuradio.fft import window
+from gnuradio.filter import firdes
+from gnuradio.wxgui import fftsink2
+from gnuradio.wxgui import scopesink2
+from grc_gnuradio import wxgui as grc_wxgui
+from optparse import OptionParser
+import osmosdr
+import time
+import wx
+from decoders import decoder_selectplus
+
+class top_block(grc_wxgui.top_block_gui):
+
+    def __init__(self):
+        grc_wxgui.top_block_gui.__init__(self, title="Top Block")
+        _icon_path = "/usr/share/icons/hicolor/32x32/apps/gnuradio-grc.png"
+        self.SetIcon(wx.Icon(_icon_path, wx.BITMAP_TYPE_ANY))
+
+        ##################################################
+        # Variables
+        ##################################################
+        self.samp_rate = samp_rate = 2000000
+        self.freq_transition = freq_transition = 10000
+        self.freq_offset = freq_offset = -80000
+        self.freq_cutoff = freq_cutoff = 50000
+        self.freq_center = freq_center = 433920000
+
+        ##################################################
+        # Blocks
+        ##################################################
+        self.wxgui_scopesink2_0 = scopesink2.scope_sink_f(
+        	self.GetWin(),
+        	title="Scope Plot",
+        	sample_rate=samp_rate,
+          size=(1920/2,1060/2),
+        	v_scale=0.2,
+        	v_offset=0.6,
+        	t_scale=0.001,
+        	ac_couple=False,
+        	xy_mode=False,
+        	num_inputs=1,
+        	trig_mode=wxgui.TRIG_MODE_AUTO,
+        	y_axis_label="signal",
+        )
+        self.wxgui_scopesink2_0.set_trigger_level(0.1)
+        self.Add(self.wxgui_scopesink2_0.win)
+        self.osmosdr_source_0 = osmosdr.source( args="numchan=" + str(1) + " " + "" )
+        self.osmosdr_source_0.set_sample_rate(samp_rate)
+        self.osmosdr_source_0.set_center_freq(freq_center, 0)
+        self.osmosdr_source_0.set_freq_corr(0, 0)
+        self.osmosdr_source_0.set_dc_offset_mode(0, 0)
+        self.osmosdr_source_0.set_iq_balance_mode(0, 0)
+        self.osmosdr_source_0.set_gain_mode(False, 0)
+        self.osmosdr_source_0.set_gain(14, 0)
+        self.osmosdr_source_0.set_if_gain(40, 0)
+        self.osmosdr_source_0.set_bb_gain(20, 0)
+        self.osmosdr_source_0.set_antenna("", 0)
+        self.osmosdr_source_0.set_bandwidth(0, 0)
+          
+        self.low_pass_filter_0 = filter.fir_filter_ccf(1, firdes.low_pass(
+        	1, samp_rate, freq_cutoff, freq_transition, firdes.WIN_HAMMING, 6.76))
+        self.dc_blocker_xx_0 = filter.dc_blocker_cc(32, True)
+        self.blocks_multiply_xx_0 = blocks.multiply_vcc(1)
+        self.blocks_complex_to_mag_0 = blocks.complex_to_mag(1)
+        self.analog_sig_source_x_0 = analog.sig_source_c(samp_rate, analog.GR_COS_WAVE, freq_offset, 1, 0)
+        self.decoder = decoder_selectplus(samp_rate)
+
+        ##################################################
+        # Connections
+        ##################################################
+        self.connect((self.analog_sig_source_x_0, 0), (self.blocks_multiply_xx_0, 0))    
+        self.connect((self.blocks_complex_to_mag_0, 0), (self.decoder, 0))    
+        self.connect((self.blocks_multiply_xx_0, 0), (self.low_pass_filter_0, 0))    
+        self.connect((self.dc_blocker_xx_0, 0), (self.blocks_multiply_xx_0, 1))    
+        self.connect((self.low_pass_filter_0, 0), (self.blocks_complex_to_mag_0, 0))    
+        self.connect((self.osmosdr_source_0, 0), (self.dc_blocker_xx_0, 0))    
+        self.connect((self.blocks_complex_to_mag_0, 0), (self.wxgui_scopesink2_0, 0))    
+        
+    def get_samp_rate(self):
+        return self.samp_rate
+
+    def set_samp_rate(self, samp_rate):
+        self.samp_rate = samp_rate
+        self.analog_sig_source_x_0.set_sampling_freq(self.samp_rate)
+        self.low_pass_filter_0.set_taps(firdes.low_pass(1, self.samp_rate, self.freq_cutoff, self.freq_transition, firdes.WIN_HAMMING, 6.76))
+        self.osmosdr_source_0.set_sample_rate(self.samp_rate)
+        self.decoder.set_sample_rate(self.samp_rate)
+        self.wxgui_scopesink2_0.set_sample_rate(self.samp_rate)
+
+    def get_freq_transition(self):
+        return self.freq_transition
+
+    def set_freq_transition(self, freq_transition):
+        self.freq_transition = freq_transition
+        self.low_pass_filter_0.set_taps(firdes.low_pass(1, self.samp_rate, self.freq_cutoff, self.freq_transition, firdes.WIN_HAMMING, 6.76))
+
+    def get_freq_offset(self):
+        return self.freq_offset
+
+    def set_freq_offset(self, freq_offset):
+        self.freq_offset = freq_offset
+        self.analog_sig_source_x_0.set_frequency(self.freq_offset)
+
+    def get_freq_cutoff(self):
+        return self.freq_cutoff
+
+    def set_freq_cutoff(self, freq_cutoff):
+        self.freq_cutoff = freq_cutoff
+        self.low_pass_filter_0.set_taps(firdes.low_pass(1, self.samp_rate, self.freq_cutoff, self.freq_transition, firdes.WIN_HAMMING, 6.76))
+
+    def get_freq_center(self):
+        return self.freq_center
+
+    def set_freq_center(self, freq_center):
+        self.freq_center = freq_center
+        self.osmosdr_source_0.set_center_freq(self.freq_center, 0)
+
+
+def main(top_block_cls=top_block, options=None):
+    tb = top_block_cls()
+    tb.Start(True)
+    tb.Wait()
+
+if __name__ == '__main__':
+    main()
---
+++ decoders.py
@@ -0,0 +1,420 @@
+# -*- coding: utf-8 -*-
+##################################################
+# GNU Radio HomeEasy Remote decoder 
+# Author: Martijn Berntsen
+# 
+##################################################
+
+from gnuradio import gr
+import numpy as np
+import sys
+import matplotlib.pyplot as plt
+
+
+##################################################
+# Protocol timing HomeEasy:
+# pulse   short :   250 us
+# space   short :   285 us
+#         long  :  1293 us
+#         xx    : 10164 us
+##################################################
+class decoder_homeeasy(gr.basic_block):
+    def __init__(self, sample_rate=32000, max_buffer_length=0.1):  # only default arguments here
+        gr.basic_block.__init__(
+            self,
+            name='HomeEasy decoder',
+            in_sig=[np.float32],
+            out_sig=[]
+        )
+        self.sample_rate = sample_rate
+        self.buffer = np.empty((0), dtype=np.int)
+        self.counts = 0
+        self.olds = None
+        self.max_buffer_length = max_buffer_length
+        self.max_buffer_size = sample_rate * max_buffer_length
+
+    def stop(self):
+        if self.olds is not None:
+          print ''
+        return True
+
+    def set_sample_rate(self, samp_rate):
+        self.sample_rate = sample_rate
+
+    def general_work(self, input_items, output_items):
+        b = np.round(input_items[0] * 10000).astype(np.int)
+        self.buffer = np.append(self.buffer, b)
+
+        refvalue = (np.min(b) + np.max(b)) // 2
+        
+        indexes = np.arange(len(self.buffer), dtype=np.int)
+        # get indexes with low values
+        i_l = indexes[self.buffer < refvalue]
+        # get holes in low signal (= pulses)
+        steps_l = np.diff(i_l)
+
+        min_width_h = self.sample_rate * 0.0002
+
+        indexes = np.arange(len(steps_l))
+        i2_l = indexes[steps_l > min_width_h]
+
+        last_sample = None
+        if len(i2_l) > 0:
+          # get edge starting points
+          t_lh = i_l[i2_l]
+          # get edge ending points
+          t_hl = i_l[i2_l + 1]
+
+          # calculate pulse widths
+          width_h = t_hl - t_lh# / (bitrate / 1000)
+          width_l = t_lh[1:] - t_hl[0:-1]# / (bitrate / 1000)
+          
+          s = ''
+          last_sample = 0
+          for i, pl in enumerate(width_l):
+            if pl < self.sample_rate * 0.0005:
+              s = s + '0'
+            elif pl < self.sample_rate * 0.005:
+              s = s + '1'
+            else:
+              if len(s) == 57:
+                self.newCode(s)
+              s = ''
+              last_sample = t_hl[i]
+          if (len(self.buffer) - t_hl[-1]) > self.sample_rate * 0.005:
+            if len(s) == 57:
+              self.newCode(s)
+            last_sample = t_hl[-1]
+        i = int(max(last_sample, len(self.buffer) - self.max_buffer_size))
+        self.buffer = self.buffer[i:]
+        self.consume(0, len(input_items[0]))
+        return 0
+
+    def newCode(self, s):
+        self.counts = self.counts + 1
+        if self.olds == s:
+          sys.stdout.write('\r%s x %d' % (self.olds, self.counts))
+          sys.stdout.flush()
+        else:
+          self.counts = 1                  
+          self.olds = s
+          sys.stdout.write('\n%s x %d' % (self.olds, self.counts))
+          sys.stdout.flush()
+
+class decoder_manchester(gr.basic_block):
+    def __init__(self, sample_rate=32000, max_buffer_length=0.1, t_short=0.0004, t_long=0.0011, t_finish=0.0075, symbolcount=26):  # only default arguments here
+        gr.basic_block.__init__(
+            self,
+            name='Manchester decoder',
+            in_sig=[np.float32],
+            out_sig=[]
+        )
+        self.sample_rate = sample_rate
+        self.buffer = np.empty((0), dtype=np.int)
+        self.counts = 0
+        self.olds = None
+        self.t_short = t_short
+        self.t_long = t_long
+        self.t_finish = t_finish
+        self.s_shortlong = sample_rate * (t_short + t_long) * 0.5
+        self.s_longfinish = sample_rate * (t_long + t_finish) * 0.5
+        self.symbolcount = symbolcount
+        self.bitcount = symbolcount * 2
+        self.max_buffer_length = max_buffer_length
+        self.max_buffer_size = sample_rate * max_buffer_length
+
+    def stop(self):
+        if self.olds is not None:
+          print ''
+        return True
+
+    def set_sample_rate(self, samp_rate):
+        self.sample_rate = sample_rate
+
+    def general_work(self, input_items, output_items):
+        b = np.round(input_items[0] * 10000).astype(np.int)
+        self.buffer = np.append(self.buffer, b)
+
+        refvalue = (np.min(b) + np.max(b)) // 2
+        
+        indexes = np.arange(len(self.buffer), dtype=np.int)
+        # get indexes with low values
+        i_l = indexes[self.buffer < refvalue]
+        # get holes in low signal (= pulses)
+        steps_l = np.diff(i_l)
+
+        min_width_h = self.sample_rate * 0.0002
+
+        indexes = np.arange(len(steps_l))
+        i2_l = indexes[steps_l > min_width_h]
+
+        last_sample = None
+        if len(i2_l) > 0:
+          # get edge starting points
+          t_lh = i_l[i2_l]
+          # get edge ending points
+          t_hl = i_l[i2_l + 1]
+
+          # calculate pulse widths
+          width_h = t_hl - t_lh# / (bitrate / 1000)
+          width_l = t_lh[1:] - t_hl[0:-1]# / (bitrate / 1000)
+          
+          s = ''
+          last_sample = 0
+          for i, ph in enumerate(width_h):
+            if i < len(width_l):
+              pl = width_l[i]
+            
+              if ph < self.s_shortlong:
+                s = s + '0'
+              else:
+                s = s + '00'
+              
+              if pl < self.s_shortlong:
+                s = s + '1'
+              elif pl < self.s_longfinish:
+                s = s + '11'
+              else:
+                if len(s) == self.bitcount:
+                  self.newCode(s)
+                s = ''
+                last_sample = t_hl[i]
+          if (len(self.buffer) - t_hl[-1]) > self.s_longfinish:
+            ph = width_h[-1]
+            if ph < self.s_shortlong:
+              s = s + '0'
+            else:
+              s = s + '00'
+            if len(s) == self.bitcount:
+              self.newCode(s)
+            last_sample = t_hl[-1]
+        i = int(max(last_sample, len(self.buffer) - self.max_buffer_size))
+        self.buffer = self.buffer[i:]
+        self.consume(0, len(input_items[0]))
+        return 0
+
+    def newCode(self, s):
+        self.counts = self.counts + 1
+        if self.olds == s:
+          sys.stdout.write('\r%s x %d' % (self.olds, self.counts))
+          sys.stdout.flush()
+        else:
+          self.counts = 1                  
+          self.olds = s
+          sys.stdout.write('\n%s x %d' % (self.olds, self.counts))
+          sys.stdout.flush()
+
+
+##################################################
+# Protocol timing SelectPlus:
+# pulse   short :   342 us
+#         long  :  1069 us
+# space   short :   397 us
+#         long  :  1125 us
+#         xx    :  7560 us
+##################################################
+class decoder_selectplus(decoder_manchester):
+    def __init__(self, sample_rate=32000, max_buffer_length=0.1):  # only default arguments here
+        gr.basic_block.__init__(
+            self,
+            name='SelectPlus decoder',
+            in_sig=[np.float32],
+            out_sig=[]
+        )
+        self.sample_rate = sample_rate
+        self.buffer = np.empty((0), dtype=np.int)
+        self.counts = 0
+        self.olds = None
+        self.max_buffer_length = max_buffer_length
+        self.max_buffer_size = sample_rate * max_buffer_length
+
+    def stop(self):
+        if self.olds is not None:
+          print ''
+        return True
+
+    def set_sample_rate(self, samp_rate):
+        self.sample_rate = sample_rate
+
+    def general_work(self, input_items, output_items):
+        b = np.round(input_items[0] * 10000).astype(np.int)
+        self.buffer = np.append(self.buffer, b)
+
+        refvalue = (np.min(b) + np.max(b)) // 2
+        
+        indexes = np.arange(len(self.buffer), dtype=np.int)
+        # get indexes with low values
+        i_l = indexes[self.buffer < refvalue]
+        # get holes in low signal (= pulses)
+        steps_l = np.diff(i_l)
+
+        min_width_h = self.sample_rate * 0.0002
+
+        indexes = np.arange(len(steps_l))
+        i2_l = indexes[steps_l > min_width_h]
+
+        last_sample = None
+        if len(i2_l) > 0:
+          # get edge starting points
+          t_lh = i_l[i2_l]
+          # get edge ending points
+          t_hl = i_l[i2_l + 1]
+
+          # calculate pulse widths
+          width_h = t_hl - t_lh# / (bitrate / 1000)
+          width_l = t_lh[1:] - t_hl[0:-1]# / (bitrate / 1000)
+          
+          s = ''
+          last_sample = 0
+          for i, ph in enumerate(width_h):
+            if i < len(width_l):
+              pl = width_l[i]
+            
+              if ph < self.sample_rate * 0.0007:
+                s = s + '1'
+              else:
+                s = s + '111'
+              
+              if pl < self.sample_rate * 0.0007:
+                s = s + '0'
+              elif pl < self.sample_rate * 0.005:
+                s = s + '000'
+              else:
+                if len(s) >= 69:
+                  self.newCode(s)
+                s = ''
+                last_sample = t_hl[i]
+          if (len(self.buffer) - t_hl[-1]) > self.sample_rate * 0.005:
+            ph = width_h[-1]
+            if ph < self.sample_rate * 0.0007:
+              s = s + '1'
+            else:
+              s = s + '111'
+            if len(s) >= 69:
+              self.newCode(s)
+            last_sample = t_hl[-1]
+        i = int(max(last_sample, len(self.buffer) - self.max_buffer_size))
+        self.buffer = self.buffer[i:]
+        self.consume(0, len(input_items[0]))
+        return 0
+
+    def newCode(self, s):
+        self.counts = self.counts + 1
+        if self.olds == s:
+          sys.stdout.write('\r%s x %d' % (self.olds, self.counts))
+          sys.stdout.flush()
+        else:
+          self.counts = 1                  
+          self.olds = s
+          sys.stdout.write('\n%s x %d' % (self.olds, self.counts))
+          sys.stdout.flush()
+
+##################################################
+# Protocol timing Elro ab440r:
+# pulse   short :   460 us
+#         long  :  1113 us
+# space   short :   188 us
+#         long  :   838 us
+#         xx    : 10000 us
+##################################################
+class decoder_elro_ab440r(gr.basic_block):
+    def __init__(self, sample_rate=32000, max_buffer_length=0.1):  # only default arguments here
+        gr.basic_block.__init__(
+            self,
+            name='Elro ab440r decoder',
+            in_sig=[np.float32],
+            out_sig=[]
+        )
+        self.sample_rate = sample_rate
+        self.buffer = np.empty((0), dtype=np.int)
+        self.counts = 0
+        self.olds = None
+        self.max_buffer_length = max_buffer_length
+        self.max_buffer_size = sample_rate * max_buffer_length
+        self.wh = np.empty((0), dtype=np.int)
+        self.wl = np.empty((0), dtype=np.int)
+
+    def stop(self):
+        if self.olds is not None:
+          print ''
+        np.save('workfileh.npy', self.wh)
+        np.save('workfilel.npy', self.wl)
+        return True
+
+    def set_sample_rate(self, samp_rate):
+        self.sample_rate = sample_rate
+
+    def general_work(self, input_items, output_items):
+        b = np.round(input_items[0] * 10000).astype(np.int)
+        self.buffer = np.append(self.buffer, b)
+
+        refvalue = (np.min(b) + np.max(b)) // 2
+        
+        indexes = np.arange(len(self.buffer), dtype=np.int)
+        # get indexes with low values
+        i_l = indexes[self.buffer < refvalue]
+        # get holes in low signal (= pulses)
+        steps_l = np.diff(i_l)
+
+        min_width_h = self.sample_rate * 0.0002
+
+        indexes = np.arange(len(steps_l))
+        i2_l = indexes[steps_l > min_width_h]
+
+        last_sample = None
+        if len(i2_l) > 0:
+          # get edge starting points
+          t_lh = i_l[i2_l]
+          # get edge ending points
+          t_hl = i_l[i2_l + 1]
+
+          # calculate pulse widths
+          width_h = t_hl - t_lh# / (bitrate / 1000)
+          width_l = t_lh[1:] - t_hl[0:-1]# / (bitrate / 1000)
+          
+          s = ''
+          oldi = 0
+          last_sample = 0
+          for i, ph in enumerate(width_h):
+            if i < len(width_l):
+              pl = width_l[i]
+              if ph < self.sample_rate * 0.0007:
+                s = s + '1'
+              else:
+                s = s + '0'
+              if pl > self.sample_rate * 0.005:
+                if len(s) == 25:
+                  self.newCode(s)
+                self.wh = np.append(self.wh, width_h[oldi:i])
+                self.wl = np.append(self.wl, width_l[oldi:i])
+                oldi = i
+                s = ''
+                last_sample = t_hl[i]
+          if (len(self.buffer) - t_hl[-1]) > self.sample_rate * 0.005:
+            ph = width_h[-1]
+            if ph < self.sample_rate * 0.0007:
+              s = s + '1'
+            else:
+              s = s + '0'
+            if len(s) == 25:
+              self.newCode(s)
+            self.wh = np.append(self.wh, width_h[oldi:-1])
+            self.wl = np.append(self.wl, width_l[oldi:-1])
+            last_sample = t_hl[-1]
+        i = int(max(last_sample, len(self.buffer) - self.max_buffer_size))
+        self.buffer = self.buffer[i:]
+        self.consume(0, len(input_items[0]))
+        return 0
+
+    def newCode(self, s):
+        s = s[1::2]
+        self.counts = self.counts + 1
+        if self.olds == s:
+          sys.stdout.write('\r%s x %d' % (self.olds, self.counts))
+          sys.stdout.flush()
+        else:
+          self.counts = 1                  
+          self.olds = s
+          sys.stdout.write('\n%s x %d' % (self.olds, self.counts))
+          sys.stdout.flush()
+
